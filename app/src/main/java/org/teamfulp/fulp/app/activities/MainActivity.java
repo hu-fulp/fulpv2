bcdb5839e6e78042be2abcc3dbe506f93e2f5a9c
royfokker <royfokker@live.nl>
2014-05-01 10:08:06 +0200

    mainactivity

--- app/src/main/java/org/teamfulp/fulp/app/activities/MainActivity.java
+++ app/src/main/java/org/teamfulp/fulp/app/activities/MainActivity.java
@@ -37,13 +37,10 @@ import org.teamfulp.fulp.app.fragments.DashboardFragment;
 import org.teamfulp.fulp.app.fragments.SummaryIncomeFragment;
 import org.teamfulp.fulp.app.fragments.SummaryInsuranceFragment;
 import org.teamfulp.fulp.app.fragments.SummarySubscriptionFragment;
-<<<<<<< HEAD
 import org.teamfulp.fulp.app.listeners.DateClickListener;
 import org.teamfulp.fulp.app.listeners.WebserviceListener;
 import org.teamfulp.fulp.app.tasks.user.UserSwitchAccountTask;
-=======
 import org.teamfulp.fulp.app.helpers.LoginSession;
->>>>>>> ceb2d9b3ffe80ff651fe6cf52725408d16a2a532
 
 import java.util.ArrayList;
 import java.util.List;
@@ -95,10 +92,7 @@ public class MainActivity extends Activity implements WebserviceListener {
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
 
-
-        Intent i = getIntent();
-<<<<<<< HEAD
-        user = (User)i.getSerializableExtra("user");
+        user = LoginSession.getInstance().getUser();
 
         if(user != null) {
             ((TextView)findViewById(R.id.label_current_user)).setText(user.getName());
@@ -134,12 +128,6 @@ public class MainActivity extends Activity implements WebserviceListener {
                                 Toast.makeText(a.getApplicationContext(), String.valueOf(selectedAccount.getId()), Toast.LENGTH_LONG).show();
 
                                 a.getUser().setCurrentAccount(selectedAccount);
-=======
-        user = LoginSession.getInstance().getUser();
-
-        if(user!= null)
-       ((TextView)findViewById(R.id.label_current_user)).setText(user.getName());
->>>>>>> ceb2d9b3ffe80ff651fe6cf52725408d16a2a532
 
                                 UserSwitchAccountTask userSwitchAccountTask = new UserSwitchAccountTask(a, a.getUser());
                                 userSwitchAccountTask.execute();
